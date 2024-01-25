6da793684dd70886b299ceee23675184afeb96f6
Crowdbotics <team@crowdbotics.com>
2024-01-25 08:23:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "grunt-risk-assesmen-45539",
+  url: "https://grunt-risk-assesmen-45539.botics.co",
+  api: "https://grunt-risk-assesmen-45539.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
